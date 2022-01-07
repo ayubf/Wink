3b4cbd071ff5cb6351278ead8126e06e058a07c9
ayubf <razortyphon@gmail.com>
2022-01-07 08:40:50 -0600

    Update Footer.tsx

--- frontend/src/components/subcomponents/Footer.tsx
+++ frontend/src/components/subcomponents/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
     return(
         <div className='footerDiv'>
-            <p>Powered by Express & React, built with NodeJS and written in TypeScript</p>
-            <p>Designed & Made by Ayub Farah</p>
+            <p>Written in TypeScript, Powered by Express & React</p>
+            <p>Designed & built by Ayub Farah</p>
             <a href="https://github.com/ayubf">Github</a> • <a href='https://www.linkedin.com/in/ayub-farah-15b278217/'>Linkedin</a>
         </div>
     )
